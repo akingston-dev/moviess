bf13d84627e0c257f867e080e3618018f4df3aba
AJ Kingston <akingstonlewis@gmail.com>
2022-03-13 13:48:30 -0700

    awd

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 
 //Components
-import Navbar from './components/Navbar';
+import Navbar from "./components/Navbar";
 
 //Pages
-import HomePage from './pages/Homepage';
-import AboutPage from './pages/Aboutpage';
-import SinglePage from './pages/Singlepage';
+import HomePage from "./pages/Homepage";
+import AboutPage from "./pages/Aboutpage";
+import SinglePage from "./pages/Singlepage";
 
 const App = () => {
-  return (
-    <Router>
-      <Navbar />
-      {/* Navbar */}
-      <div className="container">
-        <Switch>
-          <Route exact path="/" component={HomePage} />
-          <Route exact path="/about" component={AboutPage} />
-          <Route path="/singleshow/:id" component={SinglePage} />
-        </Switch>
-      </div>
-    </Router>
-  );
+	return (
+		<Router>
+			<Navbar />
+			{/* Navbar */}
+			<div className="container">
+				<Switch>
+					<Route exact path="/" component={HomePage} />
+					<Route exact path="/about" component={AboutPage} />
+					<Route path="/singleshow/:id" component={SinglePage} />
+				</Switch>
+			</div>
+		</Router>
+	);
 };
 
 export default App;
